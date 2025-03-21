ff96e6f67859399cab38e64c86e70fcf1cf3bfb3
David Grandolfo <39743735+DaGrand@users.noreply.github.com>
2025-03-21 08:48:24 -0400

    Update FirstRecon.py

--- Reconnaissance/FirstRecon.py
+++ Reconnaissance/FirstRecon.py
@@ -79,7 +79,6 @@ def scan_target(ip, results, output_file, fast_only):
         "default_scripts": "-sC",
         "service_version": "-sV",
         "light_version": "-sV --version-light",
-        "ping scan": "-sP -p",
         "quick scan": "-PN -sV --top-ports 50 --open",
         "search smb vuln": "-PN --script smb-vuln* -p139,445",
         "UDP scan": "-sU -sC -sV"
